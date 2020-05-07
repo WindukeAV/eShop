dad1b46d9eb84bf730936a38460c54b3775f02bc
Макар Главанарь <makarglavanar@MBP-Makar.sknt.ru>
2020-05-08 00:31:16 +0300

    features/architecture

--- public/js/components/counter/Counter.js
+++ public/js/components/counter/Counter.js
@@ -1,19 +1,56 @@
 'use strict';
 
 function CounterView(containerNode) {
+  var controller = new CounterController(this);
   this.containerNode = containerNode;
   this.button = this.containerNode.querySelector('[component-child-id="button"]');
   this.title = this.containerNode.querySelector('[component-child-id="title"]');
 
-  var counter = 0;
-
   this.handleClick = function() {
-    counter += 1;
+    console.log('processing click...');
+    controller.handleInrementClick();
+  };
 
+  this.setCounter = function(counter) {
     this.title.textContent = 'Счетчик ' + counter;
-  }
+  };
 
   this.handleClick = this.handleClick.bind(this);
+  this.setCounter = this.setCounter.bind(this);
 
   this.button.addEventListener('click', this.handleClick);
+}
+
+function CounterController(view) {
+  var view = view;
+  var model = new CounterModel();
+  var isProccessingIncrementClick = false;
+
+  this.handleInrementClick = function() {
+    if (isProccessingIncrementClick) return;
+
+    isProccessingIncrementClick = true;
+    model.inrementCounter(function(counter) {
+      isProccessingIncrementClick = false;
+      view.setCounter(counter);
+    });
+  };
+}
+
+function CounterModel() {
+  var counter = 0;
+
+  this.inrementCounter = function(callback) {
+    setTimeout(() => {
+      counter += 1;
+
+      if (typeof callback === 'function') {
+        callback(counter);
+      }
+    }, 500);
+  };
+
+  this.getCounter = function() {
+    return counter;
+  };
 }
