07476edd77da9efa28d1647a31317dc2191e5dea
Makar Glavanar <mr.glavanar@bk.ru>
2020-06-19 23:26:02 +0300

    supa pupa cart

--- public/js/components/cart/Cart.js
+++ public/js/components/cart/Cart.js
@@ -1,19 +1,91 @@
-function NavbarCartView(containerNode) {
-  View.call(this, containerNode);
-}
+/**
+ * @typedef CartProduct
+ * @type {object}
+ * @property {number} id
+ * @property {string} title
+ * @property {number} price
+ * @property {number} count
+ */
 
-function NavbarCartController(containerNode) {
-  
-}
-
-
-
-
-
-
+/**
+ * @typedef CartModelData
+ * @type {object}
+ * @property {Array.<CartProduct>} products
+ * @property {number} productsCount
+ * @property {number} totalPrice
+ */
 
 function CartView(containerNode) {
   View.call(this, containerNode);
-
-  // TODO: in progress
 }
+
+function CartController(view) {
+  Controller.call(this);
+}
+
+function CartModel() {
+  /**
+   * @type {CartModelData}
+   */
+  var data = {
+    products: [],
+    productsCount: 0,
+    totalPrice: 0,
+  };
+
+  /**
+   * @type {Subject}
+   */
+  var subject;
+
+  Model.call(this, data, function(_subject) {
+    subject = _subject;
+  });
+
+  /**
+   * Пересчет цены и кол-ва товаров в корзине
+   * @returns {void}
+   */
+  function recalcTotals() {
+    var productsCount = 0;
+    var totalPrice = 0;
+
+    for (var i = 0; i < data.products.length; i++) {
+      var product = data.products[i];
+
+      productsCount += 1;
+      totalPrice += product.price * product.count;
+    }
+
+    data.productsCount = productsCount;
+    data.totalPrice = totalPrice;
+  }
+
+  /**
+   * Добавление товара в корзину
+   * @param {CartProduct} product
+   * @returns {void}
+   */
+  this.addProduct = function(product) {
+    var cartProduct = data.products.find(function (_cartProduct) {
+      return _cartProduct.id === product.id;
+    });
+
+    if (cartProduct) {
+      cartProduct.count += product.count;
+    } else {
+      data.products.push(product);
+    }
+
+    recalcTotals();
+
+    subject.next(data);
+  }
+}
+
+// ДОМА
+/**
+ * - 1. запилить вьюху для корзины (верстка + подписка на модель). Нужно показать кол-ва товаров
+ * - 2. изменение кол-ва товара (с докой)
+ * - 3. удаление товара (с докой)
+ */
